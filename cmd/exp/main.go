01005263ca039075851250da88263ff6bb9876e0
Gideon Isa <innocentgideon10@gmail.com>
2022-09-24 22:17:20 +0100

    adding temaplate

---
+++ cmd/exp/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type User struct {
+	Name string
+	Age  int
+	Meta UserMeta
+}
+
+type UserMeta struct {
+	Visits int
+}
+
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	user := User{
+		Name: "Gideon Isa",
+		Age:  31,
+		Meta: UserMeta{Visits: 20},
+	}
+
+	err = t.Execute(os.Stdout, user)
+	if err != nil {
+		panic(err)
+	}
+
+}
